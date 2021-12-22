9b18eb1eeebb70e2883f8f4c873944e045efb345
shashikant193 <45946874+shashikant193@users.noreply.github.com>
2021-12-21 19:23:34 -0500

    Modified slow tests in the testing suite

--- tests/test.py
+++ tests/test.py
@@ -104,7 +104,7 @@ SYSTEMS["Tols"].append([tols["E_tol"], tols["F_tol"], tols["stress_tol"]]) # E_t
 ################################################################################################################
 SYSTEMS["systemname"].append('MoS2')
 SYSTEMS["Tags"].append(['surface','gga','potmix','nonorth','smear_fd','orient'])
-SYSTEMS["Tols"].append([tols["E_tol"], tols["F_tol"], tols["stress_tol"]]) # E_tol(Ha/atom), F_tol(Ha/Bohr), stress_tol(%)
+SYSTEMS["Tols"].append([1e-5, tols["F_tol"], tols["stress_tol"]]) # E_tol(Ha/atom), F_tol(Ha/Bohr), stress_tol(%)
 ################################################################################################################
 # SYSTEMS["systemname"].append('He16_NVKG')
 # SYSTEMS["Tags"].append(['bulk','lda','potmix','orth','smear_fd','md_nvkg','gamma'])
