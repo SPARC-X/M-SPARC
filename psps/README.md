1e42284a3ecc3fee2c450fe976c062c5854b4f01
Shashikant Kumar <shashikant@gatech.edu>
2022-11-10 15:13:41 -0500

    Updated test suite and SPMS pseudopotentials

---
+++ psps/README.md
@@ -0,0 +1,2 @@
+## SPMS table of pseudopotentials
+* See https://github.com/SPARC-X/SPMS-psps for details
--- tests/LiF_NVKG/temp_run/launch_LiF_NVKG.m
+++
@@ -1,5 +0,0 @@
- clear all 
- close all 
- addpath('./../../../src') 
- msparc('LiF_NVKG') 
- save workspace.mat
